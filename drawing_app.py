7e670ed2b289552e7c93370808976e31ae24cdeb
lubyantsev <162503570+lubyantsev@users.noreply.github.com>
2024-12-28 10:14:14 +0300

    Update drawing_app.py

--- drawing_app.py
+++ drawing_app.py
@@ -1,3 +1,5 @@
+import signal
+import sys
 import tkinter as tk
 from tkinter import colorchooser, filedialog, messagebox
 from PIL import Image, ImageDraw
@@ -5,6 +7,11 @@ from PIL import Image, ImageDraw
 
 class DrawingApp:
     def __init__(self, root):
+        """
+        Инициализация приложения рисования.
+
+        :param root: Корневое окно приложения.
+        """
         self.brush_size_var = None
         self.root = root
         self.root.title("Рисовалка с сохранением в PNG")
@@ -25,6 +32,9 @@ class DrawingApp:
         self.canvas.bind('<ButtonRelease-1>', self.reset)
 
     def setup_ui(self):
+        """
+        Настройка пользовательского интерфейса, включая кнопки управления.
+        """
         control_frame = tk.Frame(self.root)
         control_frame.pack(fill=tk.X)
 
@@ -45,9 +55,19 @@ class DrawingApp:
         brush_size_menu.pack(side=tk.LEFT)
 
     def update_brush_size(self, size):
-        self.brush_size = int(size)  # Обновляем размер кисти
+        """
+        Обновление размера кисти.
+
+        :param size: Новый размер кисти в виде строки.
+        """
+        self.brush_size = int(size)
 
     def paint(self, event):
+        """
+        Рисование на холсте при перемещении мыши с нажатой кнопкой.
+
+        :param event: Событие, содержащее информацию о положении курсора.
+        """
         if self.last_x is not None and self.last_y is not None:
             self.canvas.create_line(self.last_x, self.last_y, event.x, event.y, width=self.brush_size,
                                     fill=self.pen_color,
@@ -59,17 +79,33 @@ class DrawingApp:
         self.last_y = event.y
 
     def reset(self, event):
+        """
+        Сброс координат последней точки.
+
+        :param event: Событие, вызывающее сброс.
+        """
         self.last_x, self.last_y = None, None
 
     def clear_canvas(self):
+        """
+        Очистка холста и сброс изображения.
+        """
         self.canvas.delete("all")
         self.image = Image.new("RGB", (600, 400), "white")
         self.draw = ImageDraw.Draw(self.image)
 
     def choose_color(self):
+        """
+        Выбор цвета кисти с помощью диалогового окна выбора цвета.
+        """
         self.pen_color = colorchooser.askcolor(color=self.pen_color)[1]
 
     def save_image(self):
+        """
+        Сохранение текущего изображения в формате PNG.
+
+        Открывает диалоговое окно для выбора места сохранения файла.
+        """
         file_path = filedialog.asksaveasfilename(filetypes=[('PNG files', '*.png')])
         if file_path:
             if not file_path.endswith('.png'):
@@ -78,10 +114,24 @@ class DrawingApp:
             messagebox.showinfo("Информация", "Изображение успешно сохранено!")
 
 
+def signal_handler(_, __):
+    print("Работа прервана, рисунок не сохранён.")
+    sys.exit(0)
+
+
 def main():
+    """
+    Главная функция для запуска приложения рисования.
+    """
+    # Установка обработчика сигнала для корректного завершения приложения
+    signal.signal(signal.SIGINT, signal_handler)
+
     root = tk.Tk()
     DrawingApp(root)
-    root.mainloop()
+    try:
+        root.mainloop()
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 
 if __name__ == "__main__":
